11e457aedd6052920816ce0924f525286716a22b
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-03 19:51:25 -0300

    feat: alterando titulo do chat de acordo com o url param

--- public/assets/js/chat/chat.js
+++ public/assets/js/chat/chat.js
@@ -1,5 +1,11 @@
 import "./socketChat.js";
 
+const parametrosUrl = new URLSearchParams(window.location.search);
+const nomeSala = parametrosUrl.get("nome");
+const titulo = document.querySelector("#titulo-chat");
+titulo.textContent = nomeSala;
+
 if(!localStorage.getItem("nome")){
     window.location.href = "/";
 }
+
